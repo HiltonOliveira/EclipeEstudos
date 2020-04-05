c708b154579cc420fa6d6440e23c814381e391d0
Hilton Oliveira <44105062+HiltonOliveira@users.noreply.github.com>
2020-04-05 17:47:37 -0300

    Adicionei arquivos com Upload

---
+++ ProjetoHelloGitHub/src/br/com/hilton/git/HelloGitRemote.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package br.com.hilton.git;
+
+/**
+ * @author Hilton
+ *
+ */
+public class HelloGitRemote {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		  System.out.println("Teste Repositorio Remoto");
+	}
+
+}
